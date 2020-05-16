926b4d487b0f2958f942a1a24961802d728903b4
Cyvision <cyvision13@outlook.com>
2020-05-16 12:15:25 +0430

    UpdateActivity Part 2

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,110 +1,45 @@
 <template>
-  <article class="post">
-    <div class="activity-title-wrapper">
-        <h4 class="title-class">{{ activity.title  }}</h4>
-        <i class="fas fa-cog activity-settings"  @click="setting = !setting" ></i>
-    </div>
+  <div>
+    <ActivityItemUpdate v-if="isUpdateActive " :activity="activity" :category="categories" @toggleUpdate="changeUpdateState"/>
+    <ActivityItemDetail  v-else :activity="activity" :categories="categories" @toggleUpdate="changeUpdateState"/>
     
-    <p>{{ textUtility_capitilize(categories[activity.category].text)}}</p>
-    <p>{{ activity.notes}}</p>
-    <div class="media">
-      <div class="media-left">
-        <p class="image is-32x32">
-          <img src="../assets/user.png">
-        </p>
-      </div>
-      <div class="media-content">
-        <div class="content">
-          <p>
-            <a href="#">
-              CyVision
-              </a> updated {{ activity.updatedAt | prettyTime }} &nbsp;
-          </p>
-        </div>
-      </div>
-      <div class="media-right">
-        <!-- <span>Progress: <span :class="'color-'+ activityProgress">{{activity.progress}} %</span></span> -->
-        <span>Progress: <span :style="{'color':activityProgress}">{{activity.progress}} %</span></span>
-      </div>
-    </div>
-    <div class="activity-controll" v-if="setting">
-      <a  class="button is-warning">Edit</a>
-      <a @click="deleteActivity" class="button is-danger">Delete</a>
-    </div>
-  </article>
+  </div>
 </template>
 
 <script>
-import textUtility from '@/mixins/textutility'
+  import ActivityItemUpdate from './ActivityItemUpdate'
+  import ActivityItemDetail from  './ActivityItemDetail'
   export default {
-    data(){
-      return {
-        setting: false
-      }
-    },
-    props: {
-      categories:{
-         type : Object,
-        required: true
-      },
-      activity: {
-        type : Object,
-        required: true
-      }
-    },
-    mixins: [textUtility], //textUtility is function that extract its data and methods
-    computed:{
-      activityProgress(){
-        const progress = this.activity.progress
+   components :{
+     ActivityItemDetail,
+     ActivityItemUpdate
 
-        if(progress <= 0)
-          return 'red'
-        else if (progress <=50)
-          return 'orange'
-        else
-          return 'green'
-      }
-    },
-    methods:{
-      deleteActivity(){
-        this.$emit('ActivityDeleted',this.activity)
-      }
-    }
+   },
+   props:{
+     activity:{
+       required: true,
+       type:Object
+     },
+     categories:{
+     required:true,
+     type:Object
+     }
+   },
+   data(){
+     return {
+       isUpdateActive : false
+     }
+   },
+   methods:{
+     changeUpdateState(isUpdate){
+       this.isUpdateActive = isUpdate
+     }
+   }
+   
     
   }
 </script>
 
-<style lang="scss" scoped>
+<style  scoped>
 
-/* .color-red {
-  color: red;
-
-}
-.color-orange {
-  color: orange;
-}
-.color-green {
-  color: green;
-} */
-.post .title-class {
-  margin-bottom: 5px;
-}
-
-.activity-title{
-  margin-bottom: 5px;
-  display: inline-block;
-}
-.activity-settings {
-  float: right;
-  font-size: 22px;
-  &:hover{
-    cursor: pointer;
-  }
-}
-.activity-controll {
-  margin: 20px 0 0 0;
-  a {
-    margin-right:5px;
-  }
-}
 </style>
